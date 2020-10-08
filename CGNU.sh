511ac8e1c11fa39e0b42054ce1a308de62b97b7b
Crumpleboi <72573514+Crumpleboi@users.noreply.github.com>
2020-10-08 13:09:40 -0500

    Add files via upload

---
+++ CGNU.sh
@@ -0,0 +1,9 @@
+#The Fight Song of Grazy-Go-Nuts Univercity from H*R
+beep -l 450 -f 259.25 -n -l 450 -f 436 -n -f 346.05 -n -f 388.43 -n -f 346.05 -n -l 450 -f 290.99 -n -l 750 -f 461.83
+sleep .45
+beep -f 388.43 -n -f 388.43 -n -f 388.43 -n -l 450 -f 346.05 -n -f 326.63 -n -f 346.05 -n -l 450 -f 388.43
+sleep .45
+beep  -l 450 -f 259.25 -n -l 450 -f 436 -n -f 346.05 -n -f 388.43 -n -f 346.05 -n -l 450 -f 290.99 -n -l 750 -f 461.83
+sleep .45
+beep -f 388.43 -n -f 388.43 -n -f 388.43 -n -l 450 -f 346.05 -n -l 450 -f 326.63 -n -l 750 -f 346.05 
+echo "GO DUMPLES!!!"
